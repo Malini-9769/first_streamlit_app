f77de0ba8a0a0b42ce27160c9a977abe4eede3dc
Malini-9769 <139484671+Malini-9769@users.noreply.github.com>
2024-02-28 15:15:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
-# streamlit.text(fruityvice_response.json())
+# import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+# # streamlit.text(fruityvice_response.json())
 
 
 # # # write your own comment -what does the next line do? 
@@ -39,6 +39,11 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+# streamlit.text(fruityvice_response.json())
 
 
 # # write your own comment -what does the next line do? 
