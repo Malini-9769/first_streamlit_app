5d73af31610ad2a58381dce3d60ce36dc7e6523b
Malini-9769 <139484671+Malini-9769@users.noreply.github.com>
2024-02-28 15:48:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,6 +71,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ add_my_f
 
 streamlit.write('Thanks for adding ',add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 
 
 
